f919f8d90272079ddae384e0cef60687735944e8
Kazin Fyodor <kazinfm@gmail.com>
2021-03-23 18:10:10 +0500

    Initial commit

---
+++ Assets/CalculatorApp/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CalcApp
+{
+    public struct CalcResult
+    {
+        public readonly double Result;
+        public readonly bool Error;
+        public readonly string Status;
+        public CalcResult(double result, bool error, string status)
+        {
+            Result = result;
+            Error = error;
+            Status = status;
+        }
+    }
+
+    public static class Calculator
+    {
+        private static bool ExprValid(string expr)
+        {
+            Regex rx = new Regex("(?:[0-9 ()]+[*+/-])+[0-9 ()]+", RegexOptions.IgnoreCase);
+            MatchCollection matches = rx.Matches(expr);
+            return matches.Count > 0;
+        }
+
+        private static double Calculate(string expr)
+        {
+            var result = Convert.ToDouble(new DataTable().Compute(expr, null));
+            return result;
+        }
+
+        public static CalcResult RunExpression(string expr)
+        {
+            double result;
+            CalcResult output;
+            if (ExprValid(expr))
+            {
+                result = Calculate(expr);
+                output = new CalcResult(result, false, "Ok");
+            }
+            else
+            {
+                output = new CalcResult(0.0, true, "Invalid Expression");
+            }
+            return output;
+        }
+    }
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalcApp;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] != null && args[0].Trim() != "")
+            {
+                CalcResult calcResult = Calculator.RunExpression(args[0]);
+                if (!calcResult.Error)
+                {
+                    Console.WriteLine(calcResult.Result);
+                }
+                else
+                {
+                    Console.WriteLine("Error: " + calcResult.Status);
+                }
+            }
+            else
+                Console.WriteLine("No valid argument found.");
+
+            Console.ReadLine();
+        }
+    }
+}
